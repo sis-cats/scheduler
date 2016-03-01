bc4f1dfc5728bbad0e4c9b4d3812d52e16f6d9bc
ollie314 <mehdi.lefebvre@gmail.com>
2016-03-01 10:44:06 +0100

    Extract implementation from within the test class

--- src/test/java/fr/ca/cats/logging/oapp/scheduler/quartz/DefaultQuartzSchedulerTest.java
+++ src/test/java/fr/ca/cats/logging/oapp/scheduler/quartz/DefaultQuartzSchedulerTest.java
@@ -5,15 +5,9 @@ import static org.quartz.JobBuilder.newJob;
 import static org.quartz.TriggerBuilder.newTrigger;
 
 import java.util.Date;
-import java.util.UUID;
 
 import org.junit.Test;
-import org.quartz.Job;
-import org.quartz.JobDataMap;
 import org.quartz.JobDetail;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
-import org.quartz.JobKey;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.SchedulerFactory;
@@ -26,59 +20,20 @@ public class DefaultQuartzSchedulerTest {
 
 	private static Logger log = LoggerFactory.getLogger(DefaultQuartzSchedulerTest.class);
 
-	public static class SimpleJob implements Job {
-
-		private static final String COUNT = "count";
-
-		private Logger log = LoggerFactory.getLogger(SimpleJob.class);
-
-		private UUID uuid;
-
-		public SimpleJob() {
-			log.info("Creating a new job");
-			uuid = UUID.randomUUID();
-		}
-
-		@Override
-		public void execute(JobExecutionContext context) throws JobExecutionException {
-			log.info("Starting job execution [{}]", uuid.toString());
-			JobKey jobKey = context.getJobDetail().getKey();
-
-			if (context.isRecovering()) {
-				log.info("SimpleRecoveryJob: " + jobKey + " RECOVERING at " + new Date());
-			} else {
-				log.info("SimpleRecoveryJob: " + jobKey + " starting at " + new Date());
-			}
-
-			try {
-				log.trace("Job [{}] is now sleeping in thread [{}]", uuid.toString(), Thread.currentThread().getName());
-				Thread.sleep(2000);
-			} catch (InterruptedException e) {
-				log.error("Thread sleeping in erro with message [{}]", e.getMessage());
-			}
-			log.info("Job execution complete [{}]", uuid.toString());
-
-			JobDataMap data = context.getJobDetail().getJobDataMap();
-			int count;
-			if (data.containsKey(COUNT)) {
-				count = data.getInt(COUNT);
-			} else {
-				count = 0;
-			}
-		    ++count;
-		    data.put(COUNT, count);
-			log.info("Job {} has been execyted {} time(s) in order to be successfully completed", uuid.toString(), count);
-		}
-	}
-
 	@Test
 	public void simpleSchedule() throws SchedulerException, InterruptedException {
 		log.info("Creating a new scheduler");
 		log.info("Creating scheduler factory");
-		SchedulerFactory factory = new StdSchedulerFactory();
+		final SchedulerFactory factory = new StdSchedulerFactory();
+		final SimpleTriggerListener triggerListener = new SimpleTriggerListener();
+		final SimpleJobListener jobListener = new SimpleJobListener();
+		final SimpleSchedulerListener schedulerListener = new SimpleSchedulerListener();
 		log.info("Creating a scheduler");
 		Scheduler scheduler = factory.getScheduler();
-		scheduler.clear();
+		log.info("Adding listeners");
+		scheduler.getListenerManager().addTriggerListener(triggerListener);
+		scheduler.getListenerManager().addJobListener(jobListener);
+		scheduler.getListenerManager().addSchedulerListener(schedulerListener);
 		log.trace("Creating a new job");
 		JobDetail job = newJob(SimpleJob.class).withIdentity("job1", "group1").build();
 		log.info("Creating a new trigger");
@@ -93,7 +48,7 @@ public class DefaultQuartzSchedulerTest {
 			JobDetail aJob = newJob(SimpleJob.class).withIdentity("job" + round, "group1").build();
 			Trigger aTrigger = newTrigger().withIdentity("trigger" + round, "group1").startAt(runTime).build();
 			scheduler.scheduleJob(aJob, aTrigger);
-			Thread.sleep(3000);
+			Thread.sleep(1500);
 		}
 		log.info("All job done");
 		scheduler.shutdown(true);
